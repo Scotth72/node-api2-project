76840a163c8889a9add5fbfb30c307a3d532197c
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-06 22:18:01 -0500

    made first post request

--- Router/postRouter.js
+++ Router/postRouter.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const db = require('../data/db');
 const router = express.Router();
 
-const db = require('../data/db');
+router.post('/', (req, res) => {
+	const info = req.body;
+	console.log(req.body);
+	if (info.title && info.contents) {
+		db
+			.insert(info)
+			.then((post) => {
+				res.status(201).json(post);
+			})
+			.catch((err) => {
+				res.status(500).json({ error: 'There was an error while saving the post to the database' });
+			});
+	} else {
+		res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+	}
 
-// router.get('/', (req, res) => {
-// 	res.status(200).send(' Hello from GET');
-// });
+	// router.post('/api/post/:id/comments', (req,req) => {
 
-router.get('/', (req, res) => {
-	res.send('getting back data from router');
+	// });
+
+	router.get('/api/posts', (req, res) => {});
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const postRouter = require('./Router/postRouter');
 
 const port = 5000;
 server.use(express.json());
+
 server.use('/api/posts', postRouter);
 
-// server.get('/', (req, res) => {
-// 	res.send('Hello from Express');
-// });
+server.get('/', (req, res) => {
+	res.send('Hello from Express');
+});
 
 server.listen(port, () => console.log(`server listening on port ${port}`));
